269a434d1c4c670583e17a9eb61bd3f3c0b72e26
jinfenglin <jlin6@ND.EDU>
2018-01-23 15:31:47 -0500

    Inserting Chinese characters into the document

---
+++ src/common.py
@@ -0,0 +1,5 @@
+import os
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data')
+EASY_CLINIC_ENG = os.path.join(DATA_DIR, '2 - docs (English)')
+CSV_DIR = os.path.join(DATA_DIR, 'csv')
