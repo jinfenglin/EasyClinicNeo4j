e73ff6f627fe29e8e2225b898da07b477fc46569
jinfenglin <jlin6@ND.EDU>
2018-01-25 15:28:54 -0500

    redo the chinese insertion with sentences

--- src/chineseReplacement.py
+++ src/chineseReplacement.py
@@ -1,6 +1,6 @@
 from googletrans import Translator
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 from common import *
 import os
 import random
@@ -26,56 +26,49 @@ class ChineseReplace:
         """
 
         for path in self.paths:
-            word_set = set()
+            print("Processing {}".format(path))
             parent_dir_name = os.path.basename(os.path.dirname(path))
             file_name = os.path.basename(path)
             target_dir = os.path.join(self.pack_path, parent_dir_name)
             if not os.path.isdir(target_dir):
                 os.makedirs(target_dir)
 
-            with open(path) as fin, open(os.path.join(target_dir, file_name), 'w', encoding='utf8') as fout:
-                text = fin.read().lower()
-                tokens = word_tokenize(text)
-                word_set.update(tokens)
-                self.remove_invalid_tokens(word_set)
-                word_sampels = random.sample(word_set, int(len(word_set) * self.percentage))
-                trans_dict = self.get_chinese_word_set(word_sampels)
-                mixed_tokens = []
-                for tk in tokens:
-                    if tk in trans_dict:
-                        mixed_tokens.append(trans_dict[tk])
-                    else:
-                        mixed_tokens.append(tk)
-                fout.write(" ".join(mixed_tokens))
+            with open(      ) as fin:
+                en_sentences = fin.readlines()
+                en_sentences = [sent.strip("\n\t\r ") for sent in en_sentences]
+                zh_sentences = self.translate_sentences(en_sentences)
 
-    def remove_invalid_tokens(self, word_set):
-        for stopword in self.stopwords_set:
-            word_set.discard(stopword)
-        word_copy = list(word_set)
-        for word in word_copy:
-            if not word.isalpha():
-                word_set.remove(word)
-        return word_set
+    def gen_package_with_percentage(self, percentage, en_sentences, zh_sentences):
+        with open(os.path.join(target_dir, file_name), 'w', encoding='utf8') as fout:
+            mixed_sentences = []
+            sent_num = len(en_sentences)
+            picked_nums = random.sample([i for i in range(0, sent_num)], int(self.percentage * sent_num))
+            for i in range(0, sent_num):
+                if i in picked_nums:
+                    mixed_sentences.append(zh_sentences[i] + "\n")
+                else:
+                    mixed_sentences.append(en_sentences[i] + "\n")
+            fout.writelines(mixed_sentences)
 
     def gen_package_name(self):
         return "docs-{}%".format(round(self.percentage * 100, 2))
 
-    def get_chinese_word_set(self, en_words):
-        en_zh = dict()
+    def translate_sentences(self, en_sentences):
+        res = []
         translator = Translator()
-        for en_word in en_words:
+        for en_word in en_sentences:
             try:
-                ch_wd = translator.translate(en_word, dest='zh-cn').text
-                en_zh[en_word] = ch_wd
+                ch_sentence = translator.translate(en_word, dest='zh-cn').text
+                res.append(ch_sentence)
             except Exception as e:
                 print("Error with translating {}, sleep 60s to cool down for google translation".format(en_word))
                 sleep(60)
-        return en_zh
+        return res
 
 
 if __name__ == "__main__":
-    percent = 0.2
-    for i in range(0, 10):
+    percent = 0.0
+    for i in range(0, 1):
         percent += 0.1
         print("Processing with {}% mission...".format(percent * 100))
         replace = ChineseReplace(percent)
